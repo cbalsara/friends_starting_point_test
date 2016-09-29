c788998271c78a9ec058aaa9d8ee4cc948ae1ba8
Cyrus <cdbalsara@icloud.com>
2016-09-29 13:56:20 +0100

    this is the first question solved

---
+++ friends.rb
@@ -0,0 +1,27 @@
+# 1. For a given person, return their favourite tv show
+
+def favourite_tv_show(person_hash)
+  person_hash[:favourites][:tv_show]
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+# 2. For a given person, check if they like a particular food
+# 3. Allow a new friend to be added to a given person
+# 4. Allow a friend to be removed from a given person
+# 5. Find the total of everyone's money
+# 6. For two given people, allow the first person to loan a given value of money to the other
+# 7. Find the set of everyone's favourite food joined together
+# 8. Find people with no friends
+
+# INSANE
+# Find the people who have the same favourite tv show
---
+++ specs/friends_spec.rb
@@ -0,0 +1,108 @@
+require( 'minitest/autorun' )
+require( 'minitest/rg' )
+require_relative( '../friends' )
+
+class TestFriends < MiniTest::Test
+
+  def setup
+
+    @person1 = {
+      name: "Rick",
+      age: 12,
+      monies: 1,
+      friends: ["Jay","Keith","Marc", "Val"],
+      favourites: {
+        tv_show: "Friends",
+        things_to_eat: ["charcuterie"]
+      } 
+    }
+    
+    @person2 = {
+      name: "Jay",
+      age: 15,
+      monies: 2,
+      friends: ["Keith"],
+      favourites: {
+        tv_show: "Scrubs",
+        things_to_eat: ["soup","bread"]
+      } 
+    }
+
+    @person3 = {
+      name: "Val",
+      age: 18,
+      monies: 20,
+      friends: ["Rick", "Jay"],
+      favourites: {
+        tv_show: "Pokemon",
+        things_to_eat: ["ratatouille", "stew"]
+      } 
+    }
+
+    @person4 = {
+      name: "Keith",
+      age: 18,
+      monies: 20,
+      friends: ["Rick", "Jay", "Marc"],
+      favourites: {
+        tv_show: "Pokemon",
+        things_to_eat: ["spaghetti"]
+      } 
+    }
+
+    @person5 = {
+      name: "Marc",
+      age: 20,
+      monies: 100,
+      friends: [],
+      favourites: {
+        tv_show: "Scrubs",
+        things_to_eat: ["spinach"]
+      } 
+    }
+
+    @people = [@person1, @person2, @person3, @person4, @person5]
+
+  end
+
+  def test_name
+    assert_equal("Marc",@person5[:name])
+  end
+  #For a given person, return their favourite tv show
+
+  def test_favourite_tv_show
+    assert_equal("Scrubs", favourite_tv_show(@person5))
+  end
+
+
+
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
